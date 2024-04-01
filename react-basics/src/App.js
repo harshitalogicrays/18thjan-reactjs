87d88b704190f47c7cade961ebe20287dba2d559
harshita <harshita@logicrays.com>
2024-04-01 10:01:08 +0530

    second01apr

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import FirstFuncomp from './components/FirstFuncomp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello React</h1>
+       <FirstFuncomp mobile="99999999"></FirstFuncomp>
+        <input type="text" id="abc"/>
+       <FirstFuncomp username="Happy" address="Pune"/>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ react-basics/src/components/FirstFuncomp.jsx
@@ -0,0 +1,71 @@
+// function FirstFuncomp(){
+//     return (
+//         <h1>first fun component</h1>
+//     )
+// }
+// export default FirstFuncomp
+
+// const FirstFuncomp=()=>{
+//     return (
+//         <div>
+//         <h1>first fun component</h1>
+//         <h1>fererer</h1>
+//         </div>
+//     )
+// }
+// export default FirstFuncomp
+
+//rfce
+// import React from 'react'
+// function FirstFuncomp() {
+//   return (
+//     <div>
+//       <h1>first fun component</h1>
+//     </div>
+//   )
+// }
+// export default FirstFuncomp
+
+
+//rafc
+// import React from 'react'
+// const FirstFuncomp = () => {
+//   return (
+//     <div>
+//       <h1>first fun component arrow fun</h1>
+//     </div>
+//   )
+// }
+// export default FirstFuncomp
+
+
+
+// ==========props======
+
+// import React from 'react'
+// const FirstFuncomp = (props) => {
+//     console.log(props) //{username:"Happy" address:"Pune"}
+//   return (
+//     <div>
+//       <h1>first fun component arrow fun</h1>
+//       <h2>{props.username}</h2>
+//       <h2>{props.address}</h2>
+//       <h2>{props.mobile}</h2>
+//     </div>
+//   )
+// }
+// export default FirstFuncomp
+
+import React from 'react'
+const FirstFuncomp = ({username,address,...props}) => {
+    // console.log(props) //{username:"Happy" address:"Pune"}
+  return (
+    <div>
+      <h1>first fun component arrow fun</h1>
+      <h2>{username}</h2>
+      <h2>{address}</h2>
+      <h2>{props.mobile}</h2>
+    </div>
+  )
+}
+export default FirstFuncomp
