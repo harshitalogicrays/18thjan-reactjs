df388c6b25b5dbb9b05e98b49527833ee3e8d3f7
harshita <harshita@logicrays.com>
2024-04-30 17:03:14 +0530

    30apr

---
+++ mini-project/src/Routing.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./features/Home";
+import Login from "./features/Login";
+import Register from "./features/Register";
+import Pagenotfound from "./features/Pagenotfound";
+
+const routerPath = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children:[
+        {path:'',element:<Home/>},
+        {path:'login',element:<Login/>},
+        {path:'register',element:<Register/>},
+        {path:'*',element:<Pagenotfound/>}
+      ]
+    }
+  ]);
+
+  export default routerPath
